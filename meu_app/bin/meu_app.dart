395b06091ec5bf6245bdcf4008422a11ac9aef7c
@Vitorgabriel159 <vitor.gabriel@prodigasistemas.com.br>
2025-11-15 12:03:32 -0300

    meio initial commit

--- meu_app/bin/meu_app.dart
+++ meu_app/bin/meu_app.dart
@@ -1,5 +1,7 @@
-import 'package:meu_app/meu_app.dart' as meu_app;
+import 'dart:io';
 
-void main(List<String> arguments) {
-  print('Hello world: ${meu_app.calculate()}!');
+void main() {
+  print("oi mundo"); 
+  var entrada =  stdin.readLineSync();
+  print(entrada);
 }
